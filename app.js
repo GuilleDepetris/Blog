86904f22f2a4ad54f785a74289793f95e8a7bddd
guilledepetris <129342778+GuilleDepetris@users.noreply.github.comse>
2024-11-30 01:15:55 -0300

    Agregando login

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const connectDB = require('./server/config/db');
 
 //Connect to DB
 connectDB();
+
 //Crea una aplicacion de express
 const app = express(); //app es la instancia de tu aplicación, que manejará las rutas, middlewares y demás funcionalidades.
 //Aca se define el puerto que tu app va a escuchar,
 //si process.env.PORT esta definido se usa ese sino el puerto 5mil
 const PORT = 5000 || process.env.PORT;
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
 
 
 app.use(express.static("public"));
@@ -24,6 +27,8 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
 
 //Usas el método listen para que tu aplicación empiece a escuchar en el puerto definido
 app.listen(PORT, () => {
